abf60e10f32cdf41414d3400f085896c810baf9a
betomperez <betomperez@gmail.com>
2026-02-02 20:39:24 -0300

    Created API Layer

---
+++ AS.Users.API/Controllers/UserController.cs
@@ -0,0 +1,53 @@
+﻿using AS.Users.Application.DTO;
+using AS.Users.Application.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AS.Users.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UserController : ApiBaseController
+{
+    private readonly IUserService _userService;
+
+    public UserController(
+        IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _userService.GetAllUsersAsync();
+        return Success(users, "Lista de usuários retornada com sucesso.");
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetUser(string id)
+    {
+        var user = await _userService.GetUserByIdAsync(id);
+        return Success(user, "Usuário encontrado.");
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> CreateUser(CreateUserModel model)
+    {
+        var user = await _userService.CreateUserAsync(model);
+        return CreatedResponse(user, "Usuário criado com sucesso.");
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> PartialUpdateUser(string id, UpdateUserModel model)
+    {
+        await _userService.UpdateUserAsync(id, model);
+        return Success("Usuário atualizado com sucesso.");
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteUser(string id)
+    {
+        await _userService.DeleteUserAsync(id);
+        return NoContent();
+    }
+}
---
+++ AS.Users.API/Program.cs
@@ -0,0 +1,115 @@
+﻿using AS.Users.API.Configurations;
+using AS.Users.Application.Middleware;
+using AS.Users.Application.Services;
+using AS.Users.Application.Services.Interfaces;
+using AS.Users.Domain.Entities;
+using AS.Users.Domain.Interfaces;
+using AS.Users.Infra.Persistence.Data;
+using AS.Users.Infra.Persistence.Repositories;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// ✅ Connection String
+builder.Services.AddDbContextFactory<ASDbContext>(options =>
+{
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("UserConnection"),
+        sqlOptions => sqlOptions.EnableRetryOnFailure(
+            maxRetryCount: 5,
+            maxRetryDelay: TimeSpan.FromSeconds(10),
+            errorNumbersToAdd: null)
+    );
+});
+
+// ✅ Identity
+builder.Services.AddIdentity<User, IdentityRole>(options =>
+{
+    options.Password.RequireDigit = true;
+    options.Password.RequireLowercase = true;
+    options.Password.RequireUppercase = true;
+    options.Password.RequireNonAlphanumeric = true;
+    options.Password.RequiredLength = 4;
+    options.User.RequireUniqueEmail = true;
+})
+.AddEntityFrameworkStores<ASDbContext>()
+.AddDefaultTokenProviders();
+
+// ✅ JWT Authentication
+var jwtSettings = builder.Configuration.GetSection("JwtSettings");
+var chaveSecreta = jwtSettings["Key"];
+
+builder.Services.AddAuthentication(options =>
+{
+    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+.AddJwtBearer(options =>
+{
+    options.RequireHttpsMetadata = false;
+    options.SaveToken = true;
+    options.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = true,
+        ValidateAudience = true,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ValidIssuer = jwtSettings["Issuer"],
+        ValidAudience = jwtSettings["Audience"],
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(chaveSecreta!)),
+        ClockSkew = TimeSpan.Zero
+    };
+});
+
+
+// ✅ Serviços
+builder.Services.AddTransient<JwtService>();
+builder.Services.AddTransient<IUserRepository, UserRepository>();
+builder.Services.AddTransient<IUserService, UserService>();
+builder.Services.AddSwaggerConfiguration();
+
+
+// ✅ Swagger
+builder.Services.AddEndpointsApiExplorer();
+
+// ✅ Controllers
+builder.Services.AddControllers();
+
+builder.Logging.AddJsonConsole();
+
+
+
+var app = builder.Build();
+
+// ✅ Pipeline
+if (app.Environment.IsDevelopment() || app.Environment.IsStaging() || app.Environment.IsProduction())
+{
+    app.UseSwaggerConfiguration();
+    await using var scope = app.Services.CreateAsyncScope();
+    await using var dbContext = scope.ServiceProvider.GetRequiredService<ASDbContext>();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseDeveloperExceptionPage();
+app.UseExceptionHandler("/Error");
+app.UseHsts();
+
+
+
+app.UseHttpsRedirection();
+app.UseMiddleware<ExceptionMiddleware>();
+
+app.UseAuthentication();
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
